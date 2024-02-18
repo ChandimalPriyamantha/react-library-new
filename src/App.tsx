26756b8e1c9c007481fcf1adb9547b8d3adc1abe
M.P.C PRIYAMANTHA <95483538+ChandimalPriyamantha@users.noreply.github.com>
2024-02-18 12:21:19 +0530

    developed with okta  authantication

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,33 @@
 import React from "react";
+
 import "./App.css";
 import { Navebar } from "./layouts/NavbarAndFooter/Navebar";
 import { Footer } from "./layouts/NavbarAndFooter/Footer";
 import { HomePage } from "./layouts/HomePage";
 import { SearchBooksPage } from "./layouts/SearchBooks/SearchBooksPage";
-import { Redirect, Route, Switch } from "react-router-dom";
+import { Redirect, Route, Switch, useHistory } from "react-router-dom";
 import { BookCheckoutPage } from "./layouts/BookChekoutPage/BookChekoutPage";
+import { oktaConfig } from "./lib/oktaConfig";
+import { OktaAuth, toRelativeUrl} from '@okta/okta-auth-js';
+import { Security, LoginCallback } from "@okta/okta-react";
+import LoginWidget from "./Auth/LoginWidget";
+
+const oktaAuth = new OktaAuth(oktaConfig);
 
 export const  App = () => {
+
+  const customAuthHandler = () =>{
+         history.push('/login');
+  }
+
+  const history = useHistory();
+  
+  const restoreOriginalUri = async (__oktaAuth: any, orginalUri: any) =>{
+    history.replace(toRelativeUrl(orginalUri || '/', window.location.origin));
+  };
   return (
     <div className="d-flex flex-column min-vh-100">
+      <Security oktaAuth={oktaAuth} restoreOriginalUri={restoreOriginalUri} onAuthRequired={customAuthHandler}>
     <Navebar/>
      <div className="flex-grow-1">
     <Switch>
@@ -26,9 +44,12 @@ export const  App = () => {
     <Route path='/checkout/:bookId'>
       <BookCheckoutPage/>
     </Route>
+    <Route path='/login' render={ () => <LoginWidget config={oktaConfig}/>}/>
+    <Route path='/login/callback' component={LoginCallback}/>
     </Switch>
     </div>
     <Footer/>
+    </Security>
     </div>
 
   );
--- src/layouts/HomPage/components/LibraryServices.tsx
+++ src/layouts/HomPage/components/LibraryServices.tsx
@@ -1,6 +1,9 @@
+import { useOktaAuth } from "@okta/okta-react";
+import { Link } from "react-router-dom";
 
 
 export const LibraryServices = () => {
+    const { authState } = useOktaAuth();
     return (
         <div className='container my-5'>
             <div className='row p-4 align-items-center border shadow-lg'>
@@ -13,9 +16,19 @@ export const LibraryServices = () => {
                         send our library admin's a personal message!
                     </p>
                     <div className='d-grid gap-2 justify-content-md-start mb-4 mb-lg-3'>
-                        <a className='btn main-color btn-lg text-white' href='#'>
-                            Sign up
-                        </a>
+                    {authState?.isAuthenticated ? (
+                <Link
+                  type="button"
+                  className="btn main-color btn-lg text-white"
+                  to="#"
+                >
+                  Explor Library Services
+                </Link>
+              ) : (
+                <Link className="btn main-color btn-lg text-white" to="/login">
+                  Sign Up
+                </Link>
+              )}
                     </div>
                 </div>
                 <div className='col-lg-4 offset-lg-1 shadow-lg lost-image'></div>
